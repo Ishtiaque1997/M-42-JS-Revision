1b06dc8850343a949bf69e91010988c63c103abd
Ishtiaque1997 <ishtiaquea695@gmail.com>
2021-09-28 09:29:37 +0600

    first-commit

---
+++ fundamentals.js
@@ -0,0 +1,36 @@
+//1. how to declare a variable using let and const
+const myName='Ishtiaque Ahmed';
+let season='summer';
+season='winter'
+
+//2. 6 basic conditions in javascript
+// >,<,===,!==,<=,>=
+// multiple conditions &&,||
+if(fatherName==='rakib'||season==='rainy'){
+
+}
+else if(fatherName==='jahir'){
+
+}
+else{
+
+}
+//3.array declare
+//index,length,push
+
+const numbers=[2,34,56,78];
+numbers[0]=98;
+
+//4.for loop
+for(let i=0;i<=numbers.length;i++){
+ const number=numbers[i];
+ console.log(number)
+}
+
+//5.function
+function subtract(num1,num2){
+ const res=num1-num2;
+ return res;
+}
+const number=subtract(23,11);
+console.log(number)
